76e2515fd4dcfb9c178f1254d11e4be30b012975
jenraii <131862365+jenraii@users.noreply.github.com>
2023-09-05 12:11:29 +0300

    Bookstore lisätty

---
+++ BookstoreDatabase/src/main/java/s23/BookstoreDatabase/BookstoreDatabaseApplication.java
@@ -0,0 +1,13 @@
+package s23.BookstoreDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreDatabaseApplication.class, args);
+	}
+
+}
