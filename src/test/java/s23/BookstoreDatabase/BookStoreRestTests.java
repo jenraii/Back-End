138cd31cf4b72ffd1d670af74b84cdf6704a6ddc
Jenna Räihä <jennaraiha7@gmail.com>
2023-11-02 15:49:30 +0200

    Testaus

--- src/test/java/s23/BookstoreDatabase/BookStoreRestTests.java
+++ src/test/java/s23/BookstoreDatabase/BookStoreRestTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -29,4 +31,11 @@ public class BookStoreRestTests {
 	public void apiStatusOk() throws Exception {
 		mockMvc.perform(get("/api/books")).andExpect(status().isOk());
 	}
+	
+	@Test
+    public void shouldReturnThreeBooks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+    }
 }
